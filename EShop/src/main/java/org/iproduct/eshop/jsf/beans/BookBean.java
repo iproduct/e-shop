a3224f9a386adc3fe966f9f87db5503e24d38e10
iproduct <office@iproduct.org>
2016-08-05 14:38:11 +0300

    add books to db completed

--- EShop/src/main/java/org/iproduct/eshop/jsf/beans/BookBean.java
+++ EShop/src/main/java/org/iproduct/eshop/jsf/beans/BookBean.java
@@ -5,11 +5,15 @@
  */
 package org.iproduct.eshop.jsf.beans;
 
+import java.io.Serializable;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.New;
 import javax.inject.Inject;
+import org.iproduct.eshop.ejb.BookEJB;
 import org.iproduct.eshop.jpa.entity.Book;
 
 /**
@@ -17,12 +21,18 @@ import org.iproduct.eshop.jpa.entity.Book;
  * @author admin
  */
 @Named(value = "bookBean")
-@RequestScoped
-public class BookBean {
+@ConversationScoped
+public class BookBean implements Serializable{
 
+    @Inject
+    private Conversation conversation;
+    
     @Inject
     @New(org.iproduct.eshop.jpa.entity.Book.class)
     private Book book;
+    
+    @EJB
+    private BookEJB bookController;
     
     /**
      * Creates a new instance of Book
@@ -37,13 +47,21 @@ public class BookBean {
     public Book getBook() {
         return book;
     }
-
-    public void setBook(Book book) {
-        this.book = book;
-    }
     
     public String addBook() {
+        if(!conversation.isTransient())
+            conversation.end();
+        bookController.create(book);
         return "index";
     }
     
+    public String addAuthor() {
+        if(conversation.getId() == null)
+            conversation.begin("add-authors");
+        System.out.println("!!!! addAuthor called:" + book.getAuthors().size());
+        book.getAuthors().add("aaaaa");
+        return  "";
+    }
+    
+       
 }
