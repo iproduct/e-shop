5f362a600cdc94958e95ec51486a44ce80274509
iproduct <office@iproduct.org>
2016-08-05 12:53:54 +0300

    ejbs and jsf book feature

---
+++ EShop/src/main/java/org/iproduct/eshop/jsf/beans/Navigation.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iproduct.eshop.jsf.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author admin
+ */
+@Named(value = "navigation")
+@SessionScoped
+public class Navigation implements Serializable {
+
+    /**
+     * Creates a new instance of Navigation
+     */
+    public Navigation() {
+    }
+
+    public String getCurrentView() {
+        return FacesContext.getCurrentInstance().getViewRoot().getViewId().split("[/\\.]")[1];
+    }
+    
+    public String getActiveClass(String view) {
+        System.out.println(getCurrentView());
+        return (getCurrentView().equals(view)) ? "active" : "";
+    }
+    
+}
